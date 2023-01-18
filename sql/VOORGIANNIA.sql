58c3a3136afd47801b4530f6b94dfc449a1c83a7
Senna Oudshoorn <sennaoudshoorn@gmail.com>
2023-01-18 09:41:31 +0100

    new sql

---
+++ sql/VOORGIANNIA.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 18 jan 2023 om 09:41
+-- Serverversie: 10.4.24-MariaDB
+-- PHP-versie: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `trainingfactory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20230118080918', '2023-01-18 09:09:20', 32);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `lesson`
+--
+
+CREATE TABLE `lesson` (
+  `id` int(11) NOT NULL,
+  `sport_id` int(11) DEFAULT NULL,
+  `date` date NOT NULL,
+  `begin_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `instructor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `lesson`
+--
+
+INSERT INTO `lesson` (`id`, `sport_id`, `date`, `begin_time`, `end_time`, `instructor_id`) VALUES
+(1, 1, '2023-01-17', '13:30:00', '14:00:00', 2),
+(2, 2, '2023-01-24', '13:00:00', '14:30:00', 2),
+(3, 3, '2023-01-24', '11:00:00', '12:00:00', 2),
+(4, 1, '2023-01-17', '00:00:00', '00:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `lesson_user`
+--
+
+CREATE TABLE `lesson_user` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `lesson_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `lesson_user`
+--
+
+INSERT INTO `lesson_user` (`id`, `user_id`, `lesson_id`) VALUES
+(1, 2, 1),
+(2, 2, 1),
+(7, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `sport`
+--
+
+CREATE TABLE `sport` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `sport`
+--
+
+INSERT INTO `sport` (`id`, `name`, `image`) VALUES
+(1, 'Boxing', '/img/boxing.png'),
+(2, 'Kickboxing', '/img/kickboxing.png'),
+(3, 'MMA', '/img/mma.png');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_verified` tinyint(1) NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `postal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `is_verified`, `firstname`, `lastname`, `gender`, `adress`, `postal`, `city`) VALUES
+(1, 'guest@gmail.com', '[]', '$2y$13$we6TtQY8Rgd3sy/nZkhfAuV5GnNrWOg3rHsXaC5UqZ.KlNtScn/uO', 0, '', '', '', '', '', ''),
+(2, 'admin@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$Y4ZL3lPt8L1KwrRTR4rXT.V/ny3h2YYc.fb8fGtSRGXWrxvbz6GOG', 0, '', '', '', '', '', ''),
+(3, 'a@a.a', '[\"ROLE_USER\"]', '$2y$13$Z4e3aY90aoauGEqdvkBf/OEi.p37Xpp9jd1TuBB/D7C8mWmDap1GW', 0, '', '', '', '', '', ''),
+(4, 'a@a.aa', '[\"ROLE_USER\"]', '$2y$13$KETEyRgqVkS/r6FNStQWMOiJy7.9ByqCyD6yGmuSdIh3jp29NMEs2', 0, '', '', '', '', '', ''),
+(5, 'a@a.ab', '[\"ROLE_USER\"]', '$2y$13$XqcdLa53Lhq88bLSGy/9GepPPAner1.VNXRzlMJqWK3azH7lr96fe', 0, '', '', '', '', '', ''),
+(7, 'sennaoudshoorn@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$nJ25bc4inXe/5VJLdlwTVu1iGbfg1FB0/C95fOBpVmSd50MhbMBcq', 0, 'Senna', 'Oudshoorn', 'Man', 'Tinwerf 16', '2497PW', 'Den Haag');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexen voor tabel `lesson`
+--
+ALTER TABLE `lesson`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_F87474F3AC78BCF8` (`sport_id`),
+  ADD KEY `IDX_F87474F38C4FC193` (`instructor_id`);
+
+--
+-- Indexen voor tabel `lesson_user`
+--
+ALTER TABLE `lesson_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_B4E2102DA76ED395` (`user_id`),
+  ADD KEY `IDX_B4E2102DCDF80196` (`lesson_id`);
+
+--
+-- Indexen voor tabel `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Indexen voor tabel `sport`
+--
+ALTER TABLE `sport`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `lesson`
+--
+ALTER TABLE `lesson`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT voor een tabel `lesson_user`
+--
+ALTER TABLE `lesson_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT voor een tabel `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `sport`
+--
+ALTER TABLE `sport`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT voor een tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `lesson`
+--
+ALTER TABLE `lesson`
+  ADD CONSTRAINT `FK_F87474F38C4FC193` FOREIGN KEY (`instructor_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_F87474F3AC78BCF8` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`id`);
+
+--
+-- Beperkingen voor tabel `lesson_user`
+--
+ALTER TABLE `lesson_user`
+  ADD CONSTRAINT `FK_B4E2102DA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_B4E2102DCDF80196` FOREIGN KEY (`lesson_id`) REFERENCES `lesson` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Sport;
 use App\Form\UserType;
+use App\Repository\SportRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,14 +15,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin_index')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, SportRepository $sportRepository): Response
     {
         $users = $userRepository->findAll();
+        $sports = $sportRepository->findAll();
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'sports' => $sports,
             'users' => $users
         ]);
     }
+
+    #[Route('/sport/edit/{id}', name: 'admin_edit_sport')]
+    public function editSport(SportRepository $sportRepository, int $id, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $sport = $sportRepository->find($id);
+        $form = $this->createForm(UserType::class, $sport);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isRequired()) {
+            $entityManager->persist($sport);
+            $entityManager->flush();
+            $this->addFlash('success', 'De sport is succesvol gewijzigd');
+            return $this->redirectToRoute('admin_index');
+        }
+        return $this->render('admin/edit_user.html.twig', [
+            'controller_name' => 'AdminController',
+            'form'=> $form->createView(),
+        ]);
+    }
+
     #[Route('/user/edit/{id}', name: 'admin_edit_user')]
     public function editUser(UserRepository $userRepository, int $id, Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -38,6 +62,7 @@ class AdminController extends AbstractController
             'form'=> $form->createView(),
         ]);
     }
+
     #[Route('/user/delete/verify/{id}', name: 'admin_delete_user_page')]
     public function deleteUserPage(UserRepository $userRepository, int $id): Response
     {
@@ -46,8 +71,28 @@ class AdminController extends AbstractController
             'user' => $user
         ]);
     }
+
+    #[Route('/sport/delete/verify/{id}', name: 'admin_delete_sport_page')]
+    public function deleteSportPage(UserRepository $userRepository, int $id): Response
+    {
+        $user = $userRepository->find($id);
+        return $this->render('admin/delete_sport.html.twig', [
+            'user' => $user
+        ]);
+    }
+
     #[Route('/user/delete/{id}', name: 'admin_delete_user')]
     public function deleteUser(UserRepository $userRepository, $id, EntityManagerInterface $entityManager): Response
+    {
+        $user = $userRepository->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+        $this->addFlash('success', 'De gebruiker is succesvol verwijderd');
+        return $this->redirectToRoute('admin_index');
+    }
+
+    #[Route('/sport/delete/{id}', name: 'admin_delete_sport')]
+    public function deleteSport(UserRepository $userRepository, $id, EntityManagerInterface $entityManager): Response
     {
         $user = $userRepository->find($id);
         $entityManager->remove($user);
